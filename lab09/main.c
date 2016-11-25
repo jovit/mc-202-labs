93605ea6153066019d8deed8afc40d93d25ec694
João Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2016-11-24 22:29:53 -0200

    comments

--- lab09/main.c
+++ lab09/main.c
@@ -4,6 +4,7 @@
 #include "Heap.h"
 
 #define WHITE 0xFFFFFF
+#define RESULT_MESSAGE "Maria deve memorizar %d regioes."
 
 typedef struct {
     int width, height;
@@ -39,32 +40,32 @@ Image *read_image() {
     return img;
 }
 
-void free_image(Image *img) {
+void free_image(Image *image) {
     int i;
-    for (i = 0; i < img->height; i++) {
-        free(img->pixels[i]);
-        free(img->has_searched[i]);
+    for (i = 0; i < image->height; i++) {
+        free(image->pixels[i]);
+        free(image->has_searched[i]);
     }
 
-    free(img->pixels);
-    free(img->has_searched);
-    free(img);
+    free(image->pixels);
+    free(image->has_searched);
+    free(image);
 }
 
 
 int main(void) {
     Heap *queue;
-    HeapNode heap_node;
+    HeapNode heap_node; // auxiliar
     int current_distance;
     int current_x, current_y;
 
-    Image *img = read_image();
+    Image *image = read_image();
 
-    queue = create_heap(img->height * img->width);
+    queue = create_heap(image->height * image->width);
 
     heap_node.key = 0;
-    heap_node.x = img->white_x;
-    heap_node.y = img->white_y;
+    heap_node.x = image->white_x;
+    heap_node.y = image->white_y;
     insert(queue, heap_node);
 
     while (queue->size > 0) {
@@ -73,63 +74,66 @@ int main(void) {
         current_y = heap_node.y;
         current_distance = heap_node.key;
 
-        img->has_searched[current_y][current_x] = 1;
+        image->has_searched[current_y][current_x] = 1;
 
-        if (current_x > 0 && !img->has_searched[current_y][current_x - 1]) {
+        //check each one of the adjacent pixels, adding them to the queue with the same priority if they
+        // have the same color or with increased priority
+        if (current_x > 0 && !image->has_searched[current_y][current_x - 1]) {
             heap_node.x = current_x - 1;
             heap_node.y = current_y;
             heap_node.key = current_distance;
 
-            if (img->pixels[heap_node.y][heap_node.x] != img->pixels[current_y][current_x]) {
+            if (image->pixels[heap_node.y][heap_node.x] != image->pixels[current_y][current_x]) {
                 heap_node.key += 1;
             }
 
             insert(queue, heap_node);
         }
 
-        if (current_x < img->width - 1 && !img->has_searched[current_y][current_x + 1]) {
+        if (current_x < image->width - 1 && !image->has_searched[current_y][current_x + 1]) {
             heap_node.x = current_x + 1;
             heap_node.y = current_y;
             heap_node.key = current_distance;
 
-            if (img->pixels[heap_node.y][heap_node.x] != img->pixels[current_y][current_x]) {
+            if (image->pixels[heap_node.y][heap_node.x] != image->pixels[current_y][current_x]) {
                 heap_node.key += 1;
             }
 
             insert(queue, heap_node);
         }
 
-        if (current_y > 0 && !img->has_searched[current_y - 1][current_x]) {
+        if (current_y > 0 && !image->has_searched[current_y - 1][current_x]) {
             heap_node.x = current_x ;
             heap_node.y = current_y - 1;
             heap_node.key = current_distance;
 
-            if (img->pixels[heap_node.y][heap_node.x] != img->pixels[current_y][current_x]) {
+            if (image->pixels[heap_node.y][heap_node.x] != image->pixels[current_y][current_x]) {
                 heap_node.key += 1;
             }
 
             insert(queue, heap_node);
         }
 
-        if (current_y < img->height - 1 && !img->has_searched[current_y + 1][current_x]) {
+        if (current_y < image->height - 1 && !image->has_searched[current_y + 1][current_x]) {
             heap_node.x = current_x;
             heap_node.y = current_y + 1;
             heap_node.key = current_distance;
 
-            if (img->pixels[heap_node.y][heap_node.x] != img->pixels[current_y][current_x]) {
+            if (image->pixels[heap_node.y][heap_node.x] != image->pixels[current_y][current_x]) {
                 heap_node.key += 1;
             }
 
             insert(queue, heap_node);
         }
 
-        if (img->pixels[current_y][current_x] == WHITE && current_distance > 0) {
-            printf("Maria deve memorizar %d regioes.", current_distance - 1);
+        // if found another white area different from the initial one
+        if (image->pixels[current_y][current_x] == WHITE && current_distance > 0) {
+            printf(RESULT_MESSAGE, current_distance - 1); // the priority - 1 is equal to the distance
             break;
         }
     }
 
-    free_image(img);
+    free_image(image);
     free_heap(queue);
 
     return 0;
